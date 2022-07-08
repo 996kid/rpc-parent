6ef58fcb357c47013b87123d8d78fec1d77d9a7d
996kid <996kid@gmail.com>
2022-07-08 16:26:21 +0800

    服务端整合spring 实现注解了RpcService的bean 放入RpcServiceFactory 被调用时根据接口名称、方法名称和参数反射调用 spring生成的bean

---
+++ rpc-server/src/main/java/com/levi/rpc/ServerApplication.java
@@ -0,0 +1,17 @@
+package com.levi.rpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 996kid@gmail.com
+ * @Description ServerApplication
+ * @Date 2022/7/8 15:50
+ */
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+}
